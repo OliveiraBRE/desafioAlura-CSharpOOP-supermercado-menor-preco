a7baf76167e732e0798dc1e16c81c5327d732e13
OliveiraBRE <bruno.r.e.oliveira@gmail.com>
2022-11-14 19:28:55 -0300

    build: primeiro desafio C# OOP

---
+++ Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio01_SPMenorPreco
+{
+    public class Produto
+    {
+      public string nome;
+      public string fabricante;
+      public long codigoDeBarras;
+      public double valorUnitario;
+      public double valorCompra;
+      public int estoque;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Desafio01_SPMenorPreco;
+class Program
+{
+  static void Main(String[] args)
+  {
+    Produto leiteCondensado = new Produto();
+    leiteCondensado.nome = "Selma";
+    leiteCondensado.valorCompra = 3.42;
+    leiteCondensado.valorUnitario = 5.60;
+    leiteCondensado.fabricante = "Sergio Luiz Marangone";
+    leiteCondensado.codigoDeBarras = 9873213253425;
+    leiteCondensado.estoque = 160;
+  }
+}
